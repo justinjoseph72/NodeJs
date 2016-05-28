606e48e7a6a58e10fc61281c81aa2bbc7f1cd5b5
Justin Joseph <justin.joseph72@outlook.com>
2016-05-28 18:46:27 +0100

    Event_Emitter

---
+++ Event Programs/eventEmitterExample_1.js
@@ -0,0 +1,27 @@
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+var listener1 =function listener1(){
+console.log('listener1 executed')};
+
+var listener2 = function listener2(){
+console.log('listener 2 executed')};
+
+eventEmitter.addListener('connection',listener1);
+
+eventEmitter.on('connection',listener2);
+
+var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+console.log(eventListeners + "Listeners listening to connection event");
+
+eventEmitter.emit('connection');
+
+eventEmitter.removeListener('connection',listener1);
+console.log('Listener1 will not listen now.');
+
+eventEmitter.emit('connection');
+
+var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+console.log(eventListeners + "Listeners listening to connection event");
+
+console.log('Program ended');
